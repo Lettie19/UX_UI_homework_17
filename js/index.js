8d9eda5c9ba305334cd78ea5462c58e974224666
Lettie19 <57149975+Lettie19@users.noreply.github.com>
2021-03-29 23:07:24 -0400

    update to index

---
+++ js/index.js
@@ -0,0 +1,6 @@
+                                                                                                            // Add Event Listener Below
+$(".projects").on("click", function(){
+    $("ul").slideToggle();
+    $("image").toggleClass("rotate");
+});
+                                                                                                                                                       
